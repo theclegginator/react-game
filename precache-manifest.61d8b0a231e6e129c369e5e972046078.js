f94450f3f8ada95ce1564fdff02757f2c62a4219
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-07-12 22:51:42 -0400

    Updates

---
+++ precache-manifest.61d8b0a231e6e129c369e5e972046078.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e1cbe80fc05c8da513e2197d958522",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "df5e1c03524781ae1631",
+    "url": "/react-game/static/css/main.d7de1028.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "df5e1c03524781ae1631",
+    "url": "/react-game/static/js/main.03e81510.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "dc6cab217617891b2e6c5ac371ef4196",
+    "url": "/react-game/static/media/bowser.dc6cab21.png"
+  },
+  {
+    "revision": "ba001227692b56f7c4c3910430baa736",
+    "url": "/react-game/static/media/donkeykong.ba001227.jpg"
+  },
+  {
+    "revision": "c701856026ddadac08933777d7e5d59e",
+    "url": "/react-game/static/media/ghost.c7018560.png"
+  },
+  {
+    "revision": "c193d51be40d49d0d44c1492bc6e8e62",
+    "url": "/react-game/static/media/kirby.c193d51b.png"
+  },
+  {
+    "revision": "edfdf6814cdbbf3ea1aae7d4cebccac8",
+    "url": "/react-game/static/media/link.edfdf681.png"
+  },
+  {
+    "revision": "8687d8787872bc2493d03d4bcd27c085",
+    "url": "/react-game/static/media/luigi.8687d878.png"
+  },
+  {
+    "revision": "f8740a70e34e033ee77de5c67d7b3ad8",
+    "url": "/react-game/static/media/mario.f8740a70.png"
+  },
+  {
+    "revision": "0840b83cce9390f9a38622a9c4cc312b",
+    "url": "/react-game/static/media/megaman.0840b83c.jpeg"
+  },
+  {
+    "revision": "72346b724d71b10a933d39244ecf798c",
+    "url": "/react-game/static/media/peach.72346b72.png"
+  },
+  {
+    "revision": "6623e571ec6895713f849ed247584f76",
+    "url": "/react-game/static/media/pikachu.6623e571.jpg"
+  },
+  {
+    "revision": "a89dbd4c20e3a4f173618e02df6ef787",
+    "url": "/react-game/static/media/sonic.a89dbd4c.png"
+  }
+]);
